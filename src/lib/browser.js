00e43512e0ce331dec72c223244ff60532cc90ba
Lemon399 <1446744399@qq.com>
2021-11-25 17:25:42 +0800

    检测浏览器

--- src/lib/browser.js
+++ src/lib/browser.js
@@ -1,8 +1,13 @@
 const browsers = {};
 
 function detectBrowser() {
-  // TODO: 检测浏览器，返回代号
-  return 'via';
+  return (window.via && (window.via.cmd || window.via.openSettings)) ? 'via' :
+    (window.mbrowser && window.mbrowser.getBrowsreInfo) ? 'x' :
+    (window.bz && window.bz.addScript) ? 'bz' :
+    (window.sharkbrowser && window.sharkbrowser.installAddon) ? 'shark' :
+    (window.alook && window.alook.addon) ? 'alook' ;
+    // TODO: 其它兼容 Via 接口的浏览器是要一一列出，
+    //       还是检测到 window.via.addon 就开放安装呢？
 }
 
 ('@browser:via');
