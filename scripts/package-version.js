5d12f903435023d3c4746e4e1970d7ec050cee71
LeiLeiLei <miles_yang@foxmail.com>
2021-12-04 19:22:17 +0800

    feat: npm publish

---
+++ scripts/package-version.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+const packageJson = JSON.parse(
+  fs.readFileSync('package.json', { encoding: 'utf-8' }),
+);
+packageJson.version = process.env.GITHUB_SHA;
+fs.writeFileSync('package.json', JSON.stringify(packageJson));
