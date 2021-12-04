da45990a9a5834466168dd49190c8a91ade57e6b
LeiLeiLei <miles_yang@foxmail.com>
2021-12-04 19:31:58 +0800

    fix: publish

--- scripts/package-version.js
+++ scripts/package-version.js
@@ -3,5 +3,5 @@ const fs = require('fs');
 const packageJson = JSON.parse(
   fs.readFileSync('package.json', { encoding: 'utf-8' }),
 );
-packageJson.version = process.env.GITHUB_SHA;
+packageJson.version += `-${process.env.GITHUB_SHA}`;
 fs.writeFileSync('package.json', JSON.stringify(packageJson));
