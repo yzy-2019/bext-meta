69a7e5f17c27db5c3640e2da1963e971a2e60434
Lemon399 <1446744399@qq.com>
2022-01-23 01:03:22 +0800

    补充 BextBar 方法

--- src/lib/ui.d.ts
+++ src/lib/ui.d.ts
@@ -1,5 +1,42 @@
 declare module '@bext/ui' {
-  export function getBextBar(): HTMLDivElement;
+  interface BextBar extends HTMLDivElement {
+    queryAll(): string[];
+    query(id: string): {
+      button: HTMLButtonElement;
+      text: string;
+      textcolor: string;
+      backcolor: string;
+    };
+    add(
+      id: string,
+      opt: {
+        text?: string;
+        callback: (
+          bar: HTMLDivElement,
+          button: HTMLButtonElement,
+          e: Event,
+        ) => void;
+        textcolor?: string;
+        backcolor?: string;
+      },
+    ): number;
+    change(
+      id: string,
+      opt: {
+        text?: string;
+        callback?: (
+          bar: HTMLDivElement,
+          button: HTMLButtonElement,
+          e: Event,
+        ) => void;
+        textcolor?: string;
+        backcolor?: string;
+      },
+    ): number;
+    del(id: string): number;
+  }
+
+  export function getBextBar(): BextBar;
 
   export function toast(
     t: string,
