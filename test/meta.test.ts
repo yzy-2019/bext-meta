0ef100d9a872be973cb419da15e7b6bec9d6df7e
ikkz <miles_yang@foxmail.com>
2022-02-12 23:15:46 +0800

    chore: 迁移 tsdx

--- test/meta.test.ts
+++ test/meta.test.ts
@@ -1,24 +1,47 @@
-import { ID_RULE } from '../src/constants';
 import Ajv from 'ajv';
 import fs from 'fs';
-import { generateSchema, getProgramFromFiles } from 'typescript-json-schema';
 
 const ajv = new Ajv();
 
-const program = getProgramFromFiles(['./src/types.ts'], {
-  strictNullChecks: true,
-  esModuleInterop: true,
-});
-const validate = ajv.compile(
-  generateSchema(program, 'Meta', { required: true }),
-);
-const metas = fs.readdirSync('./meta').map((fileName) => ({
+const ID_RULE = /^[0-9]+$/;
+const META_SCHEMA = {
+  type: 'object',
+  properties: {
+    version: { type: 'string' },
+    id: { type: 'string' },
+    name: { type: 'string' },
+    tags: { type: 'array', items: { type: 'string' } },
+    synopsis: { type: 'string' },
+    match: { type: 'array', items: { type: 'string' } },
+    detail: { type: 'string' },
+    type: { type: 'string', enum: ['javascript'] },
+    source: { type: 'string' },
+    extra: { type: 'object', properties: { xMetaComment: { type: 'string' } } },
+    configSchema: {},
+    defaultConfig: {},
+  },
+  required: [
+    'detail',
+    'id',
+    'name',
+    'source',
+    'synopsis',
+    'tags',
+    'type',
+    'version',
+  ],
+  $schema: 'http://json-schema.org/draft-07/schema#',
+};
+
+const validate = ajv.compile(META_SCHEMA);
+
+const metas = fs.readdirSync('./meta').map(fileName => ({
   ...JSON.parse(fs.readFileSync(`./meta/${fileName}`, { encoding: 'utf-8' })),
   id: fileName.replace(/\.json$/, ''),
 }));
 
 describe('Meta 格式校验', () => {
-  metas.forEach((meta) => {
+  metas.forEach(meta => {
     test(meta.id, () => {
       expect(ID_RULE.test(meta.id) || meta.id === 'example').toBeTruthy();
 
