59fc6c4e90e800d5822bbf8448618a706a933de3
ikkz <miles_yang@foxmail.com>
2022-02-13 00:04:00 +0800

    fix: empty hash

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import shelljs from 'shelljs';
 
 export default (request: VercelRequest, response: VercelResponse) => {
   response.setHeader("Cache-Control", "max-age=0, s-maxage=31536000");
   response.status(200).json({
-    hash: shelljs.exec('git rev-parse HEAD').toString().replace('\n', '')
+    hash: process.env.VERCEL_GIT_COMMIT_SHA
   });
 };
