30dcea2d961529e4dc72d2204df9194b874f3b72
ikkz <miles_yang@foxmail.com>
2022-02-19 01:24:17 +0800

    feat: cors

--- api/index.ts
+++ api/index.ts
@@ -2,28 +2,20 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import meta from '../public/meta/_index.json';
 import metaTag from '../meta-tag.json';
 
-const ORIGINS = ['localhost', 'bext.ketra.fun'];
-
 export default (request: VercelRequest, response: VercelResponse) => {
-  if (
-    request.headers?.origin &&
-    ORIGINS.some(origin => request.headers.origin.includes(origin))
-  ) {
-    // cors
-    response.setHeader('Access-Control-Allow-Credentials', 'true');
-    response.setHeader('Access-Control-Allow-Origin', request.headers.origin);
-    response.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-    );
-    response.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    );
-    if (request.method === 'OPTIONS') {
-      response.status(200).end();
-      return;
-    }
+  response.setHeader('Access-Control-Allow-Credentials', 'true');
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  response.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (request.method === 'OPTIONS') {
+    response.status(200).end();
+    return;
   }
 
   response.setHeader('Cache-Control', 'max-age=0, s-maxage=31536000');
